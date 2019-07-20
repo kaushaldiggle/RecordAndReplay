cdd39ba741d154d1dabb5b052708d1f17ffff831
kaushaldiggle <50066823+kaushaldiggle@users.noreply.github.com>
2019-07-21 02:30:01 +0530

    Update TestRecord.java

--- src/main/java/com/controller/TestRecord.java
+++ src/main/java/com/controller/TestRecord.java
@@ -1,7 +1,9 @@
 package com.controller;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import com.browser.automation.SeleniumOperation;
 import com.browser.communication.CommunicationOperation;
@@ -9,33 +11,34 @@ import com.google.gson.JsonObject;
 
 public class TestRecord {
 	private URL seleniumAddress;
-	
+
 	public TestRecord() throws MalformedURLException {
 		this.seleniumAddress=new URL("http://localhost:4444/wd/hub");
 	}
-	
-	public void startTestRecord() throws MalformedURLException {
+
+	public void startTestRecord() throws IOException, InterruptedException {
 		SeleniumOperation KCBrowser=new SeleniumOperation(seleniumAddress);
 		//SeleniumOperation RPBrowser=new SeleniumOperation(seleniumAddress);
 
 		CommunicationOperation co=new CommunicationOperation();
-
-		Boolean terminte=true;
+		int recordSteps=0;
+		Boolean terminte=false;
 		do {
 			//TODO : selecting browser on which operation to be performed
 			//initial : KCBrowser
-
+			System.out.println("Start Recording Action "+(++recordSteps));
 			KCBrowser.addListners();
 
-			JsonObject bresponse=co.getResponseFromBrowser();
-
-			KCBrowser.removeListners
+			String bresponse=co.getResponseFromBrowser();
+			System.out.println("BROWSER RESPONSE : \n[\n"+bresponse+"]");
+			KCBrowser.removeListners();
 			
-			//TODO : processing the bresponse for action and selector
-
+			System.out.println("To continue press 'Y' to terminate 'N' : ");
+			Scanner in = new Scanner(System.in);
+			String s = in.nextLine();
+			if((s.toUpperCase()).equals("N"))
+				terminte=true;
 			//TODO : to determine terminate event
-			terminte=false;//continous process;
-
 		}while(!terminte);
 
 	}
