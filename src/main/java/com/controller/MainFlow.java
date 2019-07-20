571cf7f14deb0c988b91ec29ee7d172c2b1b9887
kaushaldiggle <50066823+kaushaldiggle@users.noreply.github.com>
2019-07-21 02:30:49 +0530

    Update MainFlow.java

--- src/main/java/com/controller/MainFlow.java
+++ src/main/java/com/controller/MainFlow.java
@@ -4,6 +4,7 @@ package com.controller;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -24,11 +25,16 @@ import com.google.gson.JsonObject;
 
 public class MainFlow {
 
-	public static void main(String[] args) throws MalformedURLException, FileNotFoundException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		String operation="record";//<record> and <replay>
 		if(operation.equals("record")) {
 			new TestRecord().startTestRecord();
-		}else {
+			System.out.println("Recording Completed ! , Thanks for the time .\nNow if you want to see the magic hit 'y' else 'n'");
+			Scanner in = new Scanner(System.in);
+			String s = in.nextLine();
+			if((s.toUpperCase()).equals("Y"))
+				new TestReplay();
+		}else if(operation.equals("replay")){
 			//TODO replay
 
 		}
