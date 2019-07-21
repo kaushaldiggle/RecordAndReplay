29a74adf1d2bff66c2df3753d2c009d678d118ef
kaushaldiggle <50066823+kaushaldiggle@users.noreply.github.com>
2019-07-21 20:15:31 +0530

    Update TestRecord.java

--- src/main/java/com/controller/TestRecord.java
+++ src/main/java/com/controller/TestRecord.java
@@ -19,25 +19,30 @@ public class TestRecord {
 	public void startTestRecord() throws IOException, InterruptedException {
 		SeleniumOperation KCBrowser=new SeleniumOperation(seleniumAddress);
 		//SeleniumOperation RPBrowser=new SeleniumOperation(seleniumAddress);
-
+		String tcId="C12354";
 		CommunicationOperation co=new CommunicationOperation();
 		int recordSteps=0;
 		Boolean terminte=false;
+		String actionFile="";
+		
 		do {
+			actionFile=tcId+"_"+(++recordSteps)+".json";
+			String actionFilePath="/home/kaushal/Downloads/"+actionFile;
 			//TODO : selecting browser on which operation to be performed
 			//initial : KCBrowser
-			System.out.println("Start Recording Action "+(++recordSteps));
-			KCBrowser.addListners();
+			System.out.println("Start Recording Action "+(recordSteps));
+			KCBrowser.driver.get("https://data-flair.training/blogs/read-java-console-input/#");
+			KCBrowser.addListners(actionFile);
 
-			String bresponse=co.getResponseFromBrowser();
+			String bresponse=co.getResponseFromBrowser(actionFilePath);
 			System.out.println("BROWSER RESPONSE : \n[\n"+bresponse+"]");
 			KCBrowser.removeListners();
 			
-			System.out.println("To continue press 'Y' to terminate 'N' : ");
-			Scanner in = new Scanner(System.in);
-			String s = in.nextLine();
-			if((s.toUpperCase()).equals("N"))
-				terminte=true;
+			//System.out.println("To continue press 'Y' to terminate 'N' : ");
+		//	Scanner in = new Scanner(System.in);
+		//	String s = in.nextLine();
+		//	if((s.toUpperCase()).equals("N"))
+		//		terminte=true;
 			//TODO : to determine terminate event
 		}while(!terminte);
 
