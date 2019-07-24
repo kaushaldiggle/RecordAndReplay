5ad6733e81997d61cb078cc050266d26575c8955
kaushaldiggle <50066823+kaushaldiggle@users.noreply.github.com>
2019-07-25 01:30:45 +0530

    Update CommunicationOperation.java

--- src/main/java/com/browser/communication/CommunicationOperation.java
+++ src/main/java/com/browser/communication/CommunicationOperation.java
@@ -4,6 +4,10 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import com.google.gson.JsonObject;
 
 public class CommunicationOperation {
@@ -18,7 +22,7 @@ public class CommunicationOperation {
 		//TODO : create the serverUrl
 	}
 
-	public String getResponseFromBrowser(String actionFilePath) throws InterruptedException {
+	public JSONObject getResponseFromBrowser(String actionFilePath) throws InterruptedException {
 		//TODO : wait for the response from the browser
 		//TODO : get response from the browser and return it
 		Instant start = Instant.now();
@@ -29,13 +33,25 @@ public class CommunicationOperation {
 
 			fileFound=cUtils.isFilePresent(actionFilePath);
 			if(fileFound)
+			{
+				Thread.sleep(1000);
 				dataFromFile= cUtils.getDataFromFile(actionFilePath);
+			}
+				
 			//System.out.println("File Found : "+ fileFound);
 			timelapse = Duration.between(start, Instant.now()).getSeconds();
 			//System.out.println("Execution time in seconds: "+ timelapse);
 		}while(!fileFound&&timelapse<180);//3 min elapsed for 1 event to record
 		//cUtils.deleteFileFromDownload(actionFilePath);
-		return dataFromFile;
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject = null;
+		try {
+			jsonObject = (JSONObject) jsonParser.parse(dataFromFile);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsonObject;
 
 	}
 
